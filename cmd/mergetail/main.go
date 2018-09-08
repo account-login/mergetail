d405b1fb3368a2e10077e86dd2ffdfdaffcca305
root <account-login@users.noreply.github.com>
2018-09-08 19:48:03 +0800

    config logging.

--- cmd/mergetail/main.go
+++ cmd/mergetail/main.go
@@ -101,9 +101,31 @@ func StartDebugServer(addr string) {
 	}()
 }
 
+func ConfigLogging() {
+	logger, err := log.LoggerFromConfigAsString(`
+<seelog>
+	<outputs formatid="common">
+		<console />
+	</outputs>
+    <formats>
+        <format id="common" format="%Date(2006-01-02 15:04:05.000) [%LEVEL]%t%Msg%n"/>
+    </formats>
+</seelog>`)
+	if err != nil {
+		log.Errorf("log.LoggerFromConfigAsString() failed: %v", err)
+		return
+	}
+
+	err = log.ReplaceLogger(logger)
+	if err != nil {
+		log.Errorf("log.ReplaceLogger() failed: %v", err)
+	}
+}
+
 func realMain() int {
 	defer log.Flush()
 
+	ConfigLogging()
 	StartDebugServer(":8899")
 
 	args, err := getProgArgs()
